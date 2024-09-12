58dee362e0b1fe745dfd3c8b882608c86d07eb81
KaleemAhmed123 <shaamidreez@gmail.com>
2024-09-12 11:00:14 +0530

    qns 6: most man of the match awards per year done.

--- javascript/iplDataQns/level_1.js
+++ javascript/iplDataQns/level_1.js
@@ -102,14 +102,16 @@ const topEconomicalBowler = function (matchesData, deliveriesData, year) {
     return first - second;
   });
 
-  return result;
+  const top_ten_economic = result.slice(0, 10);
+
+  return top_ten_economic;
 };
 
 // 5 => Find the number of times each team won the toss and also won the match
 
-const timesWhenTeamWonMatchAndToss = function (data) {
+const timesWhenTeamWonMatchAndToss = function (matchesData) {
   const mp = new Map();
-  for (const match of data) {
+  for (const match of matchesData) {
     const { winner, toss_winner } = match;
     if (winner === toss_winner) {
       if (!mp.has(winner)) {
@@ -130,6 +132,49 @@ const timesWhenTeamWonMatchAndToss = function (data) {
 
 // 6 => Find a player who has won the highest number of Player of the Match awards for each season
 
+// data structure  {year: [{playerName: 6}]}
+//         so Map<String, Array[Map<String, Integer>]>
+const manOfTheMatchAwardsPerSeason = function (matchesData) {
+  const mp = new Map();
+
+  for (const match of matchesData) {
+    const { season, player_of_match } = match;
+
+    if (!mp.has(season)) {
+      mp.set(season, new Map());
+    } else {
+      const yearMap = mp.get(season);
+
+      if (!yearMap.has(player_of_match)) {
+        yearMap.set(player_of_match, 1);
+      } else {
+        yearMap.set(player_of_match, yearMap.get(player_of_match) + 1);
+      }
+    }
+  }
+
+  const result = [];
+  for (const [year, playerCountArrayMap] of mp) {
+    let maxAwardCount = -1e7;
+    let playerName;
+    for (const [player, awardCount] of playerCountArrayMap) {
+      // console.log(player, typeof player, awardCount, typeof awardCount);
+      if (awardCount > maxAwardCount) {
+        maxAwardCount = awardCount;
+        playerName = player;
+      }
+    }
+
+    result.push({
+      season: year,
+      player_of_match: playerName,
+      awards_received: maxAwardCount,
+    });
+  }
+
+  return result;
+};
+
 // 7 => Find the strike rate of a batsman for each season
 
 const result1 = matchesPerYear(matchesData);
@@ -137,8 +182,10 @@ const result2 = matchesWonByTeam(matchesData);
 const result3 = extraRunsConcededInYear(matchesData, deliveriesData, "2016");
 const result4 = topEconomicalBowler(matchesData, deliveriesData, "2015");
 const result5 = timesWhenTeamWonMatchAndToss(matchesData);
-console.log("{Key => year and value => numberOfMatches} \n", result1);
-console.log("{Key => team and value => wins} \n", result2);
-console.log("{Key => team and value => extras_in_year_2016} \n", result3);
-console.log("{Key => team and value => {bowlCount, totalRuns} } \n", result4);
-console.log("{Key => team and value => {bowlCount, totalRuns} } \n", result5);
+const result6 = manOfTheMatchAwardsPerSeason(matchesData);
+// console.log("{Key => year and value => numberOfMatches} \n", result1);
+// console.log("{Key => team and value => wins} \n", result2);
+// console.log("{Key => team and value => extras_in_year_2016} \n", result3);
+// console.log("{Key => team and value => {bowlCount, totalRuns} } \n", result4);
+// console.log("{Key => team and value => {bowlCount, totalRuns} } \n", result5);
+console.log("{Key => year and value => manOfTheMatches} \n", result6);
